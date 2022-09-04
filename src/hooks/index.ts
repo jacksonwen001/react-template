fc45d35715bb6bf7690b41c5e98091305f66e7e3
jacksonwen <jacksonwen@gmail.com>
2022-09-04 20:07:09 +0800

    add hooks for store

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,13 @@
 import { useDebounce } from "@/hooks/useDebounce";
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+import type { RootState, AppDispatch } from '@/stores'
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+const useAppDispatch: () => AppDispatch = useDispatch
+const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 
 export {
-    useDebounce
+    useDebounce,
+    useAppDispatch,
+    useAppSelector
 }
